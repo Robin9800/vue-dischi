8b255af3247b04e5ff82149ef4fa04e32e042fb9
Robin9800 <robin.costanzo@gmail.com>
2022-04-11 16:57:25 +0200

    header & album list defined

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Header/>
+    <Album/>
+  </div>
+</template>
+
+<script>
+import Header from "/src/components/Header.vue"
+import Album  from "/src/components/Album.vue"
+
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Album
+
+}
+}
+</script>
+
+<style>
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>
---
+++ src/components/Album.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>album list</div>
+</template>
+
+<script>
+export default {
+    name: "albumComponent"
+
+}
+</script>
+
+<style>
+
+</style>
