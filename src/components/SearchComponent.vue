302c6c96310fb9e8fd0fc9dfe0ba7ebc3e12617a
Robin9800 <robin.costanzo@gmail.com>
2022-04-12 14:25:48 +0200

    button created

--- src/components/SearchComponent.vue
+++ src/components/SearchComponent.vue
@@ -2,20 +2,42 @@
   <div class="search">
       <input type="text" placeholder="Search" 
       v-model="inputText" 
-      @keyup.enter="$emit('search', inputText)" />
+      @keyup.enter="performSearch" />
+      <button @keyup.enter="performSearch">Cerca</button>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'SearchComponent'
+    name: 'SearchComponent',
+    data(){
+        return {
+            //Il valore di input text sarà una stringa
+            inputText: ''
+        }
+    },
+    methods: {
+        //creo la funzione che andrò ad inserire nel keyup.enter
+        performSearch(){
+            this.$emit('search',this.inputText);
+        }
+    }
 }
 </script>
 
 <style>
     .search{
         display: flex;
-        justify-content: center;
-        margin-bottom: 30px;
+        margin-bottom: 20px;
+        flex-direction: column;
+    }
+    .search input{
+        width: 200px;
+        align-self: center;
+        margin: 6px 0px;
+    }
+    .search button{
+        width: 50px;
+        align-self: center;
     }
 </style>
